731afcbe4fc28ee839851b0b424c108652e3f571
bin.zhang <1799385440.com>
2024-06-29 22:44:20 +0800

    feat：消息分发器

---
+++ Src/Lib/Common/Network/MessageDispatch.cs
@@ -0,0 +1,237 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+
+#region 消息分发器
+
+namespace Network
+{
+    /// <summary>
+    /// 消息分发器
+    /// 负责根据不同的消息类型分发消息到对应的处理器
+    /// </summary>
+    /// <typeparam name="T">消息发送者类型</typeparam>
+    public class MessageDispatch<T> : Singleton<MessageDispatch<T>>
+    {
+        #region 字段
+
+        /// <summary>
+        /// 存储消息处理器的字典，按消息类型(Type)存储对应的处理方法(Action)。
+        /// </summary>
+        private readonly Dictionary<Type, object> messageHandlers = new Dictionary<Type, object>();
+
+        #endregion
+
+        #region 构造函数
+
+        /// <summary>
+        /// 构造函数，初始化消息处理器
+        /// </summary>
+        public MessageDispatch()
+        {
+            InitializeMessageHandlers();
+        }
+
+        #endregion
+
+        #region 方法
+
+        /// <summary>
+        /// 初始化消息处理器，注册所有支持的消息类型和处理方法
+        /// </summary>
+        private void InitializeMessageHandlers()
+        {
+            RegisterHandler(new NetMessageResponseHandler<T>());
+            RegisterHandler(new NetMessageRequestHandler<T>());
+            // 如果有更多的消息类型，继续在这里注册处理器
+        }
+
+        /// <summary>
+        /// 注册消息处理器
+        /// </summary>
+        /// <typeparam name="TMessage">消息类型</typeparam>
+        /// <param name="handler">处理器实例</param>
+        private void RegisterHandler<TMessage>(IMessageHandler<T, TMessage> handler) where TMessage : class, Google.Protobuf.IMessage
+        {
+            messageHandlers[typeof(TMessage)] = handler;
+            Log.Info($"Handler registered for message type '{typeof(TMessage).Name}'");
+        }
+
+        /// <summary>
+        /// 分发消息到相应的处理器
+        /// </summary>
+        /// <typeparam name="TMessage">消息类型</typeparam>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">消息对象</param>
+        public void Dispatch<TMessage>(T sender, TMessage message) where TMessage : class, Google.Protobuf.IMessage
+        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message), "Message cannot be null");
+            }
+
+            Type messageType = typeof(TMessage);
+            if (messageHandlers.TryGetValue(messageType, out var handler))
+            {
+                ((IMessageHandler<T, TMessage>)handler).Handle(sender, message);
+                Log.Info($"Message of type '{messageType.Name}' dispatched.");
+            }
+            else
+            {
+                // 如果找不到对应的处理器，可以记录警告或者抛出异常
+                Log.Warning($"No handler registered for message type '{messageType.Name}'.");
+            }
+        }
+
+        #endregion
+    }
+}
+
+#endregion
+
+#region 消息处理接口
+
+namespace Network
+{
+    /// <summary>
+    /// 通用消息处理接口
+    /// 定义了一个处理消息的通用方法，所有消息处理器都应实现此接口。
+    /// </summary>
+    /// <typeparam name="T">消息发送者类型</typeparam>
+    /// <typeparam name="TMessage">消息类型</typeparam>
+    #region 接口定义
+    public interface IMessageHandler<T, TMessage> where TMessage : class, Google.Protobuf.IMessage
+    {
+        /// <summary>
+        /// 处理消息的方法
+        /// </summary>
+        /// <param name="sender">消息发送者，通常是网络连接或通信对象</param>
+        /// <param name="message">要处理的消息对象</param>
+        void Handle(T sender, TMessage message);
+    }
+    #endregion
+}
+
+#endregion
+
+#region 消息处理器
+
+namespace Network
+{
+    #region 响应消息处理器
+
+    /// <summary>
+    /// 响应消息处理器，处理特定类型的响应消息。
+    /// </summary>
+    /// <typeparam name="T">消息发送者类型</typeparam>
+    public class NetMessageResponseHandler<T> : IMessageHandler<T, SkillBridge.Message.NetMessageResponse>
+    {
+        /// <summary>
+        /// 处理响应消息的方法。
+        /// </summary>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">响应消息对象</param>
+        public void Handle(T sender, SkillBridge.Message.NetMessageResponse message)
+        {
+            HandleMessage(sender, message);
+        }
+
+        /// <summary>
+        /// 处理响应消息的具体逻辑。
+        /// </summary>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">响应消息对象</param>
+        private void HandleMessage(T sender, SkillBridge.Message.NetMessageResponse message)
+        {
+            // 处理响应消息的具体逻辑
+            Log.Info($"Processing response message from {sender}");
+
+            // 使用反射动态处理消息
+            HandleMessageByReflection(sender, message);
+        }
+
+        /// <summary>
+        /// 使用反射动态处理消息，将非空属性的值作为事件类型参数，通过消息分发器触发事件。
+        /// </summary>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">响应消息对象</param>
+        private void HandleMessageByReflection(T sender, SkillBridge.Message.NetMessageResponse message)
+        {
+            var messageType = message.GetType();
+            var properties = messageType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(message, null);
+                if (value != null)
+                {
+                    var eventType = value.GetType();
+                    var method = typeof(MessageDistributer<T>).GetMethod("RaiseEvent").MakeGenericMethod(eventType);
+                    method.Invoke(MessageDistributer<T>.Instance, new object[] { sender, value });
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region 请求消息处理器
+
+    /// <summary>
+    /// 请求消息处理器，处理特定类型的请求消息。
+    /// </summary>
+    /// <typeparam name="T">消息发送者类型</typeparam>
+    public class NetMessageRequestHandler<T> : IMessageHandler<T, SkillBridge.Message.NetMessageRequest>
+    {
+        /// <summary>
+        /// 处理请求消息的方法。
+        /// </summary>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">请求消息对象</param>
+        public void Handle(T sender, SkillBridge.Message.NetMessageRequest message)
+        {
+            HandleMessage(sender, message);
+        }
+
+        /// <summary>
+        /// 处理请求消息的具体逻辑。
+        /// </summary>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">请求消息对象</param>
+        private void HandleMessage(T sender, SkillBridge.Message.NetMessageRequest message)
+        {
+            // 处理请求消息的具体逻辑
+            Log.Info($"Processing request message from {sender}");
+
+            // 使用反射动态处理消息
+            HandleMessageByReflection(sender, message);
+        }
+
+        /// <summary>
+        /// 使用反射动态处理消息，将非空属性的值作为事件类型参数，通过消息分发器触发事件。
+        /// </summary>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">请求消息对象</param>
+        private void HandleMessageByReflection(T sender, SkillBridge.Message.NetMessageRequest message)
+        {
+            var messageType = message.GetType();
+            var properties = messageType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(message, null);
+                if (value != null)
+                {
+                    var eventType = value.GetType();
+                    var method = typeof(MessageDistributer<T>).GetMethod("RaiseEvent").MakeGenericMethod(eventType);
+                    method.Invoke(MessageDistributer<T>.Instance, new object[] { sender, value });
+                }
+            }
+        }
+    }
+
+    #endregion
+}
+
+#endregion
+
---
+++ Src/Lib/Common/Network/MessageDistributer.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Common;
+
+namespace Network
+{
+    /// <summary>
+    /// 消息分发器（无类型）
+    /// MessageDistributer
+    /// </summary>
+    public class MessageDistributer : MessageDistributer<object>
+    {
+    }
+
+    /// <summary>
+    /// 消息分发器（泛型版本）
+    /// MessageDistributer
+    /// </summary>
+    /// <typeparam name="T">消息发送者类型</typeparam>
+    public class MessageDistributer<T> : Singleton<MessageDistributer<T>>
+    {
+        /// <summary>
+        /// 消息参数类，封装了消息发送者和消息内容
+        /// </summary>
+        /// <typeparam name="TMessage">消息类型</typeparam>
+        class MessageArgs<TMessage> where TMessage : class, Google.Protobuf.IMessage
+        {
+            public T sender;
+            public TMessage message;
+        }
+
+        /// <summary>
+        /// 消息队列，用于存储接收到的消息。
+        /// </summary>
+        private Queue<MessageArgs<Google.Protobuf.IMessage>> messageQueue = new Queue<MessageArgs<Google.Protobuf.IMessage>>();
+
+        /// <summary>
+        /// 消息处理器委托，定义处理消息的函数。
+        /// </summary>
+        /// <typeparam name="Tm">消息类型</typeparam>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">消息内容</param>
+        public delegate void MessageHandler<Tm>(T sender, Tm message);
+
+        /// <summary>
+        /// 消息处理器字典，按消息类型存储处理器。
+        /// </summary>
+        private Dictionary<string, System.Delegate> messageHandlers = new Dictionary<string, System.Delegate>();
+
+        /// <summary>
+        /// 运行标志，用于控制消息处理器的运行状态。
+        /// </summary>
+        private bool Running = false;
+
+        /// <summary>
+        /// 线程事件，用于线程间同步。
+        /// </summary>
+        private AutoResetEvent threadEvent = new AutoResetEvent(true);
+
+        /// <summary>
+        /// 工作线程数。
+        /// </summary>
+        public int ThreadCount = 0;
+
+        /// <summary>
+        /// 活动线程数。
+        /// </summary>
+        public int ActiveThreadCount = 0;
+
+        /// <summary>
+        /// 是否抛出异常标志。
+        /// </summary>
+        public bool ThrowException = false;
+
+        /// <summary>
+        /// 消息分发器构造函数
+        /// </summary>
+        public MessageDistributer()
+        {
+        }
+
+        /// <summary>
+        /// 订阅消息处理器
+        /// </summary>
+        /// <typeparam name="Tm">消息类型</typeparam>
+        /// <param name="messageHandler">消息处理器</param>
+        public void Subscribe<Tm>(MessageHandler<Tm> messageHandler)
+        {
+            string type = typeof(Tm).Name;
+            if (!messageHandlers.ContainsKey(type))
+            {
+                messageHandlers[type] = null;
+            }
+            messageHandlers[type] = (MessageHandler<Tm>)messageHandlers[type] + messageHandler;
+        }
+
+        /// <summary>
+        /// 取消订阅消息处理器
+        /// </summary>
+        /// <typeparam name="Tm">消息类型</typeparam>
+        /// <param name="messageHandler">消息处理器</param>
+        public void Unsubscribe<Tm>(MessageHandler<Tm> messageHandler)
+        {
+            string type = typeof(Tm).Name;
+            if (!messageHandlers.ContainsKey(type))
+            {
+                messageHandlers[type] = null;
+            }
+            messageHandlers[type] = (MessageHandler<Tm>)messageHandlers[type] - messageHandler;
+        }
+
+        /// <summary>
+        /// 触发消息处理器
+        /// </summary>
+        /// <typeparam name="Tm">消息类型</typeparam>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="msg">消息内容</param>
+        public void RaiseEvent<Tm>(T sender, Tm msg)
+        {
+            string key = msg.GetType().Name;
+            if (messageHandlers.ContainsKey(key))
+            {
+                MessageHandler<Tm> handler = (MessageHandler<Tm>)messageHandlers[key];
+                if (handler != null)
+                {
+                    try
+                    {
+                        handler(sender, msg);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Log.ErrorFormat("Message handler exception:{0}, {1}, {2}, {3}", ex.InnerException, ex.Message, ex.Source, ex.StackTrace);
+                        if (ThrowException)
+                            throw ex;
+                    }
+                }
+                else
+                {
+                    Log.Warning("No handler subscribed for {0}" + msg.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <typeparam name="TMessage">消息类型</typeparam>
+        /// <param name="sender">消息发送者</param>
+        /// <param name="message">消息内容</param>
+        public void ReceiveMessage<TMessage>(T sender, TMessage message) where TMessage : class, Google.Protobuf.IMessage
+        {
+            // 创建消息参数对象并入队
+            var messageArgs = new MessageArgs<TMessage>
+            {
+                sender = sender,
+                message = message
+            };
+            this.messageQueue.Enqueue(messageArgs as MessageArgs<Google.Protobuf.IMessage>); // 显式转换为基类类型
+
+            // 设置线程事件，通知处理器有新消息
+            threadEvent.Set();
+        }
+
+
+
+        /// <summary>
+        /// 清空消息队列
+        /// </summary>
+        public void Clear()
+        {
+            this.messageQueue.Clear();
+        }
+
+        /// <summary>
+        /// 一次性分发队列中的所有消息
+        /// </summary>
+        public void Distribute()
+        {
+            if (this.messageQueue.Count == 0)
+            {
+                return;
+            }
+
+            while (this.messageQueue.Count > 0)
+            {
+                dynamic package = this.messageQueue.Dequeue();
+                if (package.message != null)
+                {
+                    // 使用通用的 Dispatch 方法来分发消息
+                    MessageDispatch<T>.Instance.Dispatch(package.sender, package.message);
+                    //if (package.message is SkillBridge.Message.NetMessageRequest)
+                    //{
+                    //    var request = package.message as SkillBridge.Message.NetMessageRequest;
+                    //    MessageDispatch<T>.Instance.Dispatch(package.sender, request);
+                    //}
+                    //else if (package.message is SkillBridge.Message.NetMessageResponse)
+                    //{
+                    //    var response = package.message as SkillBridge.Message.NetMessageResponse;
+                    //    MessageDispatch<T>.Instance.Dispatch(package.sender, response);
+                    //}
+                    //// 可以根据需要添加其他消息类型的处理
+                }
+            }
+        }
+
+        /// <summary>
+        /// 启动消息处理器
+        /// [多线程模式]
+        /// </summary>
+        /// <param name="ThreadNum">工作线程数</param>
+        public void Start(int ThreadNum)
+        {
+            this.ThreadCount = ThreadNum;
+            if (this.ThreadCount < 1) this.ThreadCount = 1;
+            if (this.ThreadCount > 1000) this.ThreadCount = 1000;
+            Running = true;
+            for (int i = 0; i < this.ThreadCount; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(MessageDistribute));
+            }
+            while (ActiveThreadCount < this.ThreadCount)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// 停止消息处理器
+        /// [多线程模式]
+        /// </summary>
+        public void Stop()
+        {
+            Running = false;
+            this.messageQueue.Clear();
+            while (ActiveThreadCount > 0)
+            {
+                threadEvent.Set();
+            }
+            Thread.Sleep(100);
+        }
+
+        /// <summary>
+        /// 消息处理线程
+        /// [多线程模式]
+        /// </summary>
+        /// <param name="stateInfo">线程状态信息</param>
+        private void MessageDistribute(Object stateInfo)
+        {
+            Log.Warning("MessageDistribute thread start");
+            try
+            {
+                ActiveThreadCount = Interlocked.Increment(ref ActiveThreadCount);
+                while (Running)
+                {
+                    if (this.messageQueue.Count == 0)
+                    {
+                        threadEvent.WaitOne();
+                        continue;
+                    }
+                    dynamic package = this.messageQueue.Dequeue();
+                    if (package.message != null)
+                    {
+                        // 使用通用的 Dispatch 方法来分发消息
+                        MessageDispatch<T>.Instance.Dispatch(package.sender, package.message);
+                        //if (package.message is SkillBridge.Message.NetMessageRequest)
+                        //{
+                        //    var request = package.message as SkillBridge.Message.NetMessageRequest;
+                        //    MessageDispatch<T>.Instance.Dispatch(package.sender, request);
+                        //}
+                        //else if (package.message is SkillBridge.Message.NetMessageResponse)
+                        //{
+                        //    var response = package.message as SkillBridge.Message.NetMessageResponse;
+                        //    MessageDispatch<T>.Instance.Dispatch(package.sender, response);
+                        //}
+                        //// 可以根据需要添加其他消息类型的处理
+                    }
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                ActiveThreadCount = Interlocked.Decrement(ref ActiveThreadCount);
+                Log.Warning("MessageDistribute thread end");
+            }
+        }
+    }
+}
