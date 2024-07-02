6133fc6333b51e60344f633fc40b93e8380981cf
bin.zhang <1799385440.com>
2024-07-02 22:04:14 +0800

    feat：客户端生成日志

--- Src/Client/Assets/Scripts/ClientNetworkLauncher.cs
+++ Src/Client/Assets/Scripts/ClientNetworkLauncher.cs
@@ -1,11 +1,32 @@
+using log4net;
 using Network;
 using SkillBridge.Message;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ClientNetworkLauncher : MonoBehaviour
 {
+    private void Awake()
+    {
+        // 获取 log4net.xml 文件的正确路径
+        string projectRootPath = Application.dataPath;  // 获取项目Assets目录
+        string log4netConfigPath = Path.Combine(projectRootPath, "log4net.xml").Replace("\\", "/");  // 构建 log4net.xml 文件的完整路径
+
+        // 检查并创建日志目录
+        string logDirectory = Path.Combine(projectRootPath, "Log").Replace("\\", "/");
+        if (!Directory.Exists(logDirectory))
+        {
+            Directory.CreateDirectory(logDirectory);
+        }
+
+        // 配置 log4net
+        log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(log4netConfigPath));
+
+        UnityLogger.Init();
+    }
+
     private void Start()
     {
         NetClient.Instance.Init("127.0.0.1", 8000);
